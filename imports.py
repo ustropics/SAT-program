ba6952f65200d9e8c355a44db638a90573b66559
USTropics <ustropics@gmail.com>
2024-10-26 08:54:43 -0400

    Add plotting functionality

--- imports.py
+++ imports.py
@@ -9,4 +9,5 @@ def pip_install():
     install_and_import('satpy')
     install_and_import('pyspectral')
     install_and_import('panel')
+    install_and_import('jupyter_bokeh')
     print("Python modules and libaries installed successfully... Have fun plotting!")
