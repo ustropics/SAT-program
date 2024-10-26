675415f7a2d3d32e312448c538f387b939a56c9d
USTropics <ustropics@gmail.com>
2024-10-26 07:25:14 -0400

    Add download functionality for files

--- config.py
+++ config.py
@@ -1,5 +1,18 @@
+## Filename: config.py
+## Description: This file contains configuration settings for the application.
+
+# Set initial variables to get data
 sat_sel = 16
 sat_domain = 'F'
 sat_product = 'ABI-L2-MCMIP'
 start_time = '2024-10-08 16:00'
 end_time = '2024-10-08 16:10'
+
+# Set the list of available satellite composites
+sat_recipes = ['true_color_nocorr', 'true_color_reproduction_corr', 'geo_color', 'true_color', 'true_color_crefl',
+               'cimss_true_color_sunz_rayleigh', 'cimss_true_color_sunz', 'cimss_true_color', 
+               'true_color_reproduction_corr', 'true_color_reporduction_uncorr', 'true_color_reproduction']
+
+# Set directory files
+nc_dir = 'data/nc/'
+img_dir = 'media/img/'
---
+++ data_get.py
@@ -0,0 +1,23 @@
+## Filename: data_get.py
+## Description: This file contains functions for getting data.
+
+# Import necessary libraries, functions, and config options
+from config import *
+from goes2go import GOES
+
+
+
+## GET DATA
+
+def get_sat_data():
+    
+    # Get the satellite product
+    G = GOES(satellite=sat_sel, product=sat_product, domain=sat_domain)
+
+    # Get the data file list
+    df = G.df(start=start_time, end=end_time)
+
+    # Donwload the data
+    G.timerange(start=start_time, end=end_time, save_dir=nc_dir)
+    
+    return df
